9c7013b382135d62ac243023dc69cda4a4555486
Renee Nordholm <reneenordholm@gmail.com>
2020-04-02 11:28:22 -0500

    challenge setup

---
+++ ruby/in-progress/DuplicateEncoder.rb
@@ -0,0 +1,22 @@
+# The goal of this exercise is to convert a string to a new string 
+# where each character in the new string is "(" if that character 
+# appears only once in the original string, or ")" if that character 
+# appears more than once in the original string. 
+
+# Ignore capitalization when determining if a character is a duplicate.
+
+# Examples
+# "din"      =>  "((("
+# "recede"   =>  "()()()"
+# "Success"  =>  ")())())"
+# "(( @"     =>  "))((" 
+
+def duplicate_encode(word)
+    #your code here
+end
+
+# Tests
+# duplicate_encode("din")
+# duplicate_encode("recede")
+# duplicate_encode("Success")
+# duplicate_encode("(( @")
